23841249ac257b98db274a15a8eb132eb4b077ef
clydeharold <ch.paronda@gmail.com>
2021-09-25 16:20:47 +0800

    Added Explicit intent

--- app/src/main/java/com/example/afternoonintent/MainActivity.java
+++ app/src/main/java/com/example/afternoonintent/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.afternoonintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button cmdExplicit;
+    EditText txtMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cmdExplicit = findViewById(R.id.cmdExplicit);
+        txtMessage = findViewById(R.id.txtMessage);
+
+        cmdExplicit.setOnClickListener(startSecondActivity);
     }
+
+    View.OnClickListener startSecondActivity = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String msg = txtMessage.getText().toString();
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+
+            intent.putExtra("message", msg);
+
+            startActivity(intent);
+
+        }
+    };
 }
